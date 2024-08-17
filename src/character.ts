1ac141c4c0f7c9fe400d88cd04c29ffa7fe6a555
Michael Hendrick <mhendric@bu.edu>
2024-08-16 20:36:29 -0400

    character stubs and first commit

---
+++ src/character.ts
@@ -0,0 +1,37 @@
+
+// 
+abstract class Character {
+    name: string;
+    ascii_face: string = 'TODO';
+    constructor(name: string) {
+        this.name = name;
+    }
+    getName(): string {
+        return this.name;
+    }
+
+    getFace(): string {
+        return this.ascii_face;
+    }
+}
+
+export class MainCharacter extends Character {
+}
+
+export class CompanionCharacter extends Character {
+    is_current_companion: boolean;
+    constructor(name: string, is_current_companion: boolean) {
+        super(name);
+        this.is_current_companion = is_current_companion;
+    }
+
+    setIsCurrentCompanion(is_current_companion: boolean): void {
+        this.is_current_companion = is_current_companion;
+    }
+    getIsCurrentCompanion(): boolean {
+        return this.is_current_companion;
+    }   
+}
+
+export class NonPlayableCharacter extends Character {
+}
--- src/counter.ts
+++ src/counter.ts
@@ -1,8 +1,12 @@
+
+import { MainCharacter } from './character.ts'
 export function setupCounter(element: HTMLButtonElement) {
   let counter = 0
   const setCounter = (count: number) => {
     counter = count
     element.innerHTML = `count is ${counter}`
+    const sample_character = new MainCharacter("bob");
+    console.log(sample_character.name);
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
